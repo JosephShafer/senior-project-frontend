9362e98df4e6ef569189948a37a9ec28828bac62
Joseph Shafer <joeyshafer96@gmail.com>
2020-10-23 21:44:57 -0700

    commit

--- App.js
+++ App.js
@@ -21,6 +21,10 @@ function DetailsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
