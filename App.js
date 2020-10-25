a8efbf6f907fd86b7743d594207a4804e2396113
Joseph Shafer <joeyshafer96@gmail.com>
2020-10-24 22:16:27 -0700

    Work in progress, made boilerplate login

--- App.js
+++ App.js
@@ -1,29 +1,66 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, Image, TextInput, StyleSheet  } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Snap & Go placeholder front Screen</Text>
       <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        title="Go to Login"
+        onPress={() => navigation.navigate('Login')}
       />
     </View>
   );
 }
 
-function DetailsScreen() {
+function Login() {
+  const [value, onChangeUserText] = React.useState('');
+  const [password, onChangePasswordText] = React.useState('');
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
+    <View style={{flex: 1, flexDirection: 'column', justifyContent: "space-between"}}>
+      
+      <Text style={{ flex: .5, alignItems: "center", textAlign: "center", fontSize: 50}}>Snap & Go</Text>
+
+        <Image
+          style={styles.placeholderImage}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+
+
+      <View style={{flex: 1, flexDirection: "column", alignItems: "center"}}>
+        <TextInput
+          focus
+          autoCompleteType={"username"}
+          style={styles.textBox}
+          onChangeText={text => onChangeUserText(text)}
+          value={value}
+          placeholder={'Username'}
+        />
+
+        <TextInput
+          secureTextEntry
+          style={styles.textBox}
+          onChangeText={text => onChangePasswordText(text)}
+          value={password}
+          placeholder={'Password'}
+        />
+      </View>
+      <View style={{flex: .5}}>
+        <Button 
+          title="Login"
+        />
+      </View>
     </View>
   );
-}
+};
 
 
 
@@ -34,10 +71,30 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }}/> 
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+
+const styles = StyleSheet.create({
+  placeholderImage: {
+    flex: 2,
+    marginTop: 10,
+    height: undefined,
+    width: undefined,
+    resizeMode: 'contain',
+  },
+  textBox: {
+    height: 40,
+    width: '90%',
+    marginTop: 20,
+    alignItems: 'center',
+    borderColor: 'gray',
+    borderWidth: 1
+  }
+
+});
 
 export default App;
