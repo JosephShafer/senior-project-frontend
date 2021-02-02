787d97075161a8cacd0a4853355b7c72a7e2d5d8
Joseph Shafer <joeyshafer96@gmail.com>
2021-02-02 09:46:56 -0800

    added linking to external webpages from list component dummy data

--- screens/ResultsScreen.js
+++ screens/ResultsScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, TextInput, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import * as Linking from 'expo-linking';
 
 function ResultsScreen() {
   const [results, setResultText] = useState([]);
@@ -20,16 +21,29 @@ function ResultsScreen() {
     getJson();
   }, []);
 
+  const pressHandler = (id) => {
+    console.log(id);
+    Linking.openURL('http://google.com');
+  }
+
   // https://www.youtube.com/watch?v=IuYo009yc8w
   return (
     <View>
+    {/* TODO: add on scroll down disappear, on scrollup reappear */}
+      <TextInput
+        style={styles.myTextInput}
+        placeholder={'Filter By'}
+      ></TextInput>
       <FlatList
+        style={styles.myList}
         data={results}
         keyExtractor={(item) => 'key' + item.id}
         renderItem={({ item }) =>
-          <Text>
+        <TouchableOpacity onPress={() => pressHandler(item.id)}>
+          <Text style={styles.item}>
             {`Title: ${item.title}`}
           </Text>
+        </TouchableOpacity>
         }
       >
       </FlatList>
@@ -38,6 +52,20 @@ function ResultsScreen() {
 };
 
 // Todo style better
-
+const styles = StyleSheet.create({
+  myTextInput: {
+    height: 50,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginTop: 10
+  },
+  myList: {
+    marginTop: 20,
+  },
+  item: {
+    marginTop: 10,
+    fontSize: 20,
+  }
+})
 
 export default ResultsScreen;
