3c715fecd4654e74671309ff62fef91db83e1d89
Joseph Shafer <joeyshafer96@gmail.com>
2021-02-15 22:12:58 -0800

    templated out part of account page

---
+++ screens/Account.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, FlatList, Button } from 'react-native';
+
+
+// Dummy info that will be replaced by auth0/database,
+const accountInfo = { fName: 'Joseph', lName: "Shafer" }
+const lastFewSearches = [{name: 'yarn', location: 'target'}, {name: 'scissors', location: 'amazon'}, {name: 'glitter', location: 'target'}]
+
+
+function Account() {
+
+    // This is a dummy bool for when we can extract login success
+    const [loginSuccess, setLoginSuccess] = useState(false);
+
+    return (
+        showAccount(loginSuccess)
+    );
+};
+
+function showAccount(loginSuccess) {
+    if (loginSuccess) {
+        return (
+            <View>
+                <Text style={styles.myText}>
+                    Welcome Back {accountInfo.fName} {accountInfo.lName}
+                </Text>
+                <Text>Here are your last few searches</Text>
+                <FlatList
+                    data={lastFewSearches}
+                    keyExtractor={(x, i) => i}
+                    renderItem={({ item }) =>
+                        <Text>
+                            {item.name} bought from {item.location}
+                        </Text>
+                    }
+                >
+                </FlatList>
+            </View>
+        );
+    } else {
+        return (
+            <View>
+                <Text style={styles.myText}>
+                    No Account Logged in
+                </Text>
+                <Text>If you would like to create an account, click here:</Text>
+                <Button
+                    title="Login"
+                    onPress={callAuth0}
+                ></Button>
+            </View>
+        );
+    }
+
+}
+
+function callAuth0(){
+    console.log("auth0 login called");
+}
+
+const styles = StyleSheet.create({
+    myText: {
+        paddingTop: 3,
+        fontSize: 30
+    }
+});
+
+export default Account;
