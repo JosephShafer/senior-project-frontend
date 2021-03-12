759b52fc4ff927a3a2243d27fca4e35f4526912f
Joseph Shafer <joeyshafer96@gmail.com>
2021-03-11 22:05:33 -0800

    redid UI for login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
-import { Button, View, Text, TextInput, Image, StyleSheet } from 'react-native';
+import { ImageBackground, Button, View, Text, TextInput, Image, StyleSheet, KeyboardAvoidingView, ScrollView, TouchableOpacity } from 'react-native';
+import { Asset, useAssets } from 'expo-asset';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
+import config from '../config.json';
 
 function Login() {
   const [value, onChangeUserText] = useState('');
   const [password, onChangePasswordText] = useState('');
+  const [backgroundImage, error] = useAssets([require('../assets/craftyImage.jpeg')]);
+
+  let [fontsLoaded] = useFonts({
+    'Redressed-Regular' : require('../assets/fonts/Redressed-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  
 
   // really basic HTTP request to the EC instance I got going
   const submitInfo = async () => {
-    const url = "http://ec2-3-83-108-10.compute-1.amazonaws.com:3000/";
+    const url = config.AWS.url;
     try {
       let response = await fetch(url);
       let fetchData = await response.text();
@@ -18,64 +33,123 @@ function Login() {
       console.error(error);
     }
   }
-
+ 
+  
   return (
-    <View style={{ flex: 1, flexDirection: 'column', justifyContent: "space-between", paddingTop: 25}}>
+    <ImageBackground style={styles.placeholderImage} source={require('../assets/craftyImage.jpeg')}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.loginContainer}>
 
-      <Text style={{ flex: .5, alignItems: "center", textAlign: "center", fontSize: 50 }}>Snap & Go</Text>
+        <Text style={styles.titleText}>Snap & Go</Text>
 
-      <Image
-        style={styles.placeholderImage}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
-      />
+        <ScrollView contentContainerStyle={styles.textBoxContainer}>
+          <TextInput
+            focus
+            autoCompleteType={"username"}
+            style={styles.textBox}
+            onChangeText={text => onChangeUserText(text)}
+            value={value}
+            placeholder={'Username'}
+            onPress={() => setShowImage(false)}
+          />
+          <TextInput
+            secureTextEntry
+            style={styles.textBox}
+            onChangeText={text => onChangePasswordText(text)}
+            value={password}
+            placeholder={'Password'}
+          />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              onPress={submitInfo}
+            >
+              <View style={styles.button}>
+                <Text style={styles.buttonText}> Login </Text>
+              </View>
+            </TouchableOpacity>
 
 
-      <View style={{ flex: 1, flexDirection: "column", alignItems: "center" }}>
-        <TextInput
-          focus
-          autoCompleteType={"username"}
-          style={styles.textBox}
-          onChangeText={text => onChangeUserText(text)}
-          value={value}
-          placeholder={'Username'}
-        />
+            <TouchableOpacity
+              onPress={submitInfo}
+            >
+              <View style={styles.button}>
+                <Text style={styles.buttonText}> Sign Up </Text>
+              </View>
+            </TouchableOpacity>
 
-        <TextInput
-          secureTextEntry
-          style={styles.textBox}
-          onChangeText={text => onChangePasswordText(text)}
-          value={password}
-          placeholder={'Password'}
-        />
-      </View>
-      <View style={{ flex: .5 }}>
-        <Button
-          title="Login"
-          onPress={submitInfo}
-        />
-      </View>
-    </View>
+
+            <TouchableOpacity
+              onPress={submitInfo}
+            >
+              <View style={styles.button}>
+                <Text style={styles.buttonText}> Sign In With Google </Text>
+              </View>
+            </TouchableOpacity>
+            </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  loginContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: "space-between",
+    paddingTop: 25,
+  },
+  titleText: {
+    fontFamily: 'Redressed-Regular',
+    alignItems: "center",
+    textAlign: "center",
+    fontSize: 80,
+    paddingBottom: 0,
+    color: 'white',
+    textShadowColor: 'rgba(50, 50, 50, 0.95)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 20
+  },
   placeholderImage: {
-    flex: 2,
-    marginTop: 10,
+    flex: 1,
+    marginTop: 0,
     height: undefined,
     width: undefined,
     resizeMode: 'contain',
+    
+  },
+
+  textBoxContainer: {
+    flex: 1.5,
+    alignItems: 'center',
+    flexDirection: "column",
+    paddingVertical: 20
   },
   textBox: {
     height: 40,
     width: '90%',
     marginTop: 20,
     alignItems: 'center',
-    borderColor: 'gray',
-    borderWidth: 1
+    backgroundColor: 'white',
+    borderRadius: 20,
   },
+  buttonContainer : {
+    flex: 1
+  },
+  button: {
+    marginTop: 10,
+    borderRadius: 20,
+    paddingVertical: 14,
+    paddingHorizontal: 90,
+    backgroundColor: 'blue'
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+
 });
 
 export default Login;
